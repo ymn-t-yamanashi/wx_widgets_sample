bb58ff92268ca069a5659713894c7969d0daed63
ymn-t-yamanashi <ae86bg5@yahoo.co.jp>
2026-05-22 18:29:47 +0900

    feat: elixir_3d_galleryプロジェクトを作成しpre-commitを設定

---
+++ elixir_3d_gallery/lib/elixir3d_gallery.ex
@@ -0,0 +1,18 @@
+defmodule Elixir3dGallery do
+  @moduledoc """
+  Documentation for `Elixir3dGallery`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Elixir3dGallery.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ elixir_3d_gallery/test/elixir3d_gallery_test.exs
@@ -0,0 +1,8 @@
+defmodule Elixir3dGalleryTest do
+  use ExUnit.Case
+  doctest Elixir3dGallery
+
+  test "greets the world" do
+    assert Elixir3dGallery.hello() == :world
+  end
+end
